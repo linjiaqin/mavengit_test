005a148534ba231bcf0617cd6f16eaec881f3f7c
linjiaqin <1263457581@qq.com>
2018-09-11 17:15:44 +0800

    "修改A.java,增加了I am linjiaqin

--- mavengit_test/src/main/java/mavengit_test/A.java
+++ mavengit_test/src/main/java/mavengit_test/A.java
@@ -3,6 +3,6 @@ package mavengit_test;
 public class A {
 	public static void main(String[] args)
 	{
-		System.out.println("hello world emm");
+		System.out.println("hello world emm, I am linjiaqin");
 	}
 }
