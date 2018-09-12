94a4c28438a742f8b98ba9bc2575d040c6dfd358
linjiaqin <1263457581@qq.com>
2018-09-12 10:31:56 +0800

    我在文件系统修改了这个文件，看看eclipse是否修改，结果发现会自动修改

--- mavengit_test/src/main/java/mavengit_test/A.java
+++ mavengit_test/src/main/java/mavengit_test/A.java
@@ -3,6 +3,6 @@ package mavengit_test;
 public class A {
 	public static void main(String[] args)
 	{
-		System.out.println("hello world emm, I am linjiaqin");
+		System.out.println("hello world emm, 我修改了这个文件，看看eclipse是否修改");
 	}
 }
