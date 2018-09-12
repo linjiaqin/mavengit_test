358a353706385ad05da77bfa0e78c70c24541ea8
linjiaqin <1263457581@qq.com>
2018-09-12 10:45:50 +0800

    修改A.java

--- mavengit_test/src/main/java/mavengit_test/A.java
+++ mavengit_test/src/main/java/mavengit_test/A.java
@@ -3,6 +3,6 @@ package mavengit_test;
 public class A {
 	public static void main(String[] args)
 	{
-		System.out.println("hello world emm, 我修改了这个文件，看看eclipse是否修改");
+		System.out.println("hello world emm");
 	}
 }
